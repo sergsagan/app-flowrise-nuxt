daa45e3dbfb2487cb86fc77517d00793203af5aa
sergsagan <sergey.martynuk@gmail.com>
2024-08-13 15:44:28 +0200

    Initial commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,11 @@
+module.exports = {
+    content: ['./slices/**/*.{js,ts,vue}'],
+    theme: {
+        extend: {
+            fontFamily: {
+                body: ['Nunito Sans Variable', 'sans-serif'],
+                heading: ['Nunito Sans', 'sans-serif'],
+            }
+        }
+    }
+}
