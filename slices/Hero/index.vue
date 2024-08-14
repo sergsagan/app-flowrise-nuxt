bb6122951ce3087157f8affa516918202477a1f7
sergsagan <sergey.martynuk@gmail.com>
2024-08-14 17:40:49 +0200

    update index

---
+++ slices/Hero/index.vue
@@ -0,0 +1,50 @@
+<script setup lang="ts">
+import { type Content } from "@prismicio/client";
+
+// The array passed to `getSliceComponentProps` is purely optional.
+// Consider it as a visual hint for you when templating your slice.
+defineProps(
+  getSliceComponentProps<Content.HeroSlice>([
+    "slice",
+    "index",
+    "slices",
+    "context",
+  ]),
+);
+</script>
+
+<template>
+  <Bounded
+    :data-slice-type="slice.slice_type"
+    :data-slice-variation="slice.variation"
+  >
+    <div
+      class="grid grid-cols-1 place-items-center"
+      :class="{'md:grid-cols-2' : slice.variation === 'horizontal'}"
+    >
+      <div
+        class="flex flex-col"
+        :class="{' items-center text-center': slice.variation === 'default'}"
+      >
+        <PrismicRichText
+          :field="slice.primary.heading"
+          class="heading heading--xl max-w-3xl mb-4 md:mb-8"
+        />
+        <PrismicRichText
+          :field="slice.primary.body"
+          class="mb-4 max-w-2xl md:mb-8 text-2xl font-normal leading-10 font-body text-slate-600"
+        />
+        <PrismicLink
+          :field="slice.primary.button_link"
+          class="button mb-8 md:mb-10"
+        >
+          {{ slice.primary.button_text }}
+        </PrismicLink>
+      </div>
+      <PrismicImage
+        :field="slice.primary.image"
+        class="max-w-4xl w-ful drop-shadow-xl"
+      />
+    </div>
+  </Bounded>
+</template>
